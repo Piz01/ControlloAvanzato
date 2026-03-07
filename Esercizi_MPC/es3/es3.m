636f06c657dc313b66191b3b091e9bb3bbb7d420
Matteo Talè <s1121445@studenti.univpm.it>
2026-03-07 15:15:13 +0100

    pulizia es3

--- Esercizi_MPC/es3/es3.m
+++ Esercizi_MPC/es3/es3.m
@@ -2,18 +2,8 @@
 clear; clc; close all;
 
 % --- Parametri del Processo ---
-num = 1.3;
-den = [1 -0.7];
-Ts = 3;              % Tempo di campionamento (sec)
-
-Hp = 3;              % Orizzonte di predizione (9 sec)
-Hu = 3;              % Orizzonte di controllo (3 mosse)
-set_point = 2.5;     % Traiettoria di set-point s(k+i)
-Tref = 9;            % Costante di tempo della reference trajectory
-
-u_prec = 0.3;        % u(k-1)
-y_prec = 1.3;        % y(k-1)
-y_attuale = 1.3;     % y(k)
+num = 1.3; den = [1 -0.7]; Ts = 3; Hp = 3; Hu = 3; set_point = 2.5; Tref = 9;
+u_prec = 0.3; y_prec = 1.3; y_attuale = 1.3;
 
 fprintf('--- ESERCIZIO 3: Calcolo matrice ottima all''istante k ---\n');
 
@@ -48,7 +38,6 @@ for j = 2:Hu
 end
 
 % 4. Creazione della Matrice Dinamica S (triangolare inferiore)
-% Poiché abbiamo 3 incognite e 3 equazioni
 Matrice_Step = [
     step_response(1), 0,                0;
     step_response(2), step_response(1), 0;
@@ -60,28 +49,17 @@ Matrice_Step = [
 % Imponendo Y_predetta = R_ref, troviamo Delta_U:
 Delta_U = Matrice_Step \ (R_ref - Y_free);
 
-% Estraiamo la prima mossa, che è l'unica che verrà effettivamente applicata (Receding Horizon)
-delta_u_hat_1 = Delta_U(1);
-delta_u_hat_2 = Delta_U(2);
-delta_u_hat_3 = Delta_U(3);
+u_hat_1 = u_prec + Delta_U(1);
+u_hat_2 = u_hat_1 + Delta_U(2);
+u_hat_3 = u_hat_2 + Delta_U(3);
 
-u_hat_1 = u_prec + delta_u_hat_1;
-u_hat_2 = u_hat_1 + delta_u_hat_2;
-u_hat_3 = u_hat_2 + delta_u_hat_3;
-
-% Stampe a video dei risultati
-fprintf('Traiettoria di riferimento R:\n');
-disp(R_ref);
-fprintf('Risposta libera Y_free:\n');
-disp(Y_free);
-fprintf('Matrice Dinamica S:\n');
-disp(Matrice_Step);
-fprintf('Vettore delle variazioni di controllo ottime Delta_U:\n');
-disp(Delta_U);
+fprintf('Traiettoria di riferimento R:\n'); disp(R_ref);
+fprintf('Risposta libera Y_free:\n'); disp(Y_free);
+fprintf('Matrice Dinamica S:\n'); disp(Matrice_Step);
+fprintf('Vettore delle variazioni di controllo ottime Delta_U:\n'); disp(Delta_U);
 fprintf('----> u(k|k) OTTIMA DA APPLICARE: %.4f\n\n', u_hat_1);
 
-%% --- GRAFICI ESERCIZIO 3 (Mostrati a video ma non salvati) ---
-% Calcolo dell'uscita predetta totale
+%% --- GRAFICI ESERCIZIO 3 ---
 Y_pred = Y_free + Matrice_Step * Delta_U;
 
 % Generazione degli assi per il plot (da k-1 a k+3)
@@ -92,20 +70,31 @@ y_plot = [y_prec, y_attuale, Y_pred(1), Y_pred(2), Y_pred(3)];
 u_plot = [u_prec, u_hat_1, u_hat_2, u_hat_3, u_hat_3]; % L'ultimo valore si mantiene costante
 ref_plot = [y_prec, y_attuale, ref_P1, ref_P2, ref_P3];
 
-figure('Name', 'MPC Esercizio 3 - Predizione k');
+fig1 = create_fig('MPC Esercizio 3 - Predizione k');
 subplot(2,1,1);
-plot(time_pred, y_plot, '-o', 'LineWidth', 1.5); hold on;
-plot(time_pred, ref_plot, '--r', 'LineWidth', 1.5);
-xlabel('Tempo (s)'); ylabel('Uscita y(k+i|k)');
-title('Predizione dell''uscita all''istante k (Hp=3, Hu=3)');
-legend('y predetta', 'Reference Trajectory', 'Location', 'SouthEast');
-grid on;
+plot(time_pred, y_plot, '-o', 'Color', [0 0.447 0.741], 'LineWidth', 1.5, 'MarkerSize', 6, 'MarkerFaceColor', [0 0.447 0.741]); hold on;
+plot(time_pred, ref_plot, '--', 'Color', [0.85 0.325 0.098], 'LineWidth', 1.5);
+format_ax('Tempo (s)', 'Uscita y(k+i|k)', 'Predizione dell''uscita all''istante k (Hp=3, Hu=3)', {'y predetta', 'Reference Trajectory'});
 
 subplot(2,1,2);
-stairs(time_pred, u_plot, '-s', 'LineWidth', 1.5);
-xlabel('Tempo (s)'); ylabel('Ingresso u(k+i|k)');
-title('Sequenza di controllo ottima (Hu=3)');
-grid on;
+stairs(time_pred, u_plot, '-s', 'Color', [0.466 0.674 0.188], 'LineWidth', 1.5, 'MarkerSize', 6, 'MarkerFaceColor', [0.466 0.674 0.188]);
+format_ax('Tempo (s)', 'Ingresso u(k+i|k)', 'Sequenza di controllo ottima (Hu=3)', {});
 
-% Salva SOLO il file .mat nel Workspace per la professoressa
 save('Dati_Esercizio_3.mat');
+
+%% --- FUNZIONI LOCALI ---
+function fig = create_fig(name)
+    fig = figure('Name', name, 'Color', 'w', 'Position', [100, 100, 800, 600]);
+end
+
+function format_ax(xl, yl, tit, leg_num)
+    xlabel(xl, 'FontSize', 11, 'FontName', 'Helvetica', 'Color', 'k');
+    ylabel(yl, 'FontSize', 11, 'FontName', 'Helvetica', 'Color', 'k');
+    title({tit; ''}, 'FontSize', 12, 'FontName', 'Helvetica', 'FontWeight', 'bold', 'Color', 'k');
+    if ~isempty(leg_num)
+        leg = legend(leg_num, 'Location', 'southeast', 'FontSize', 10, 'Box', 'off');
+        set(leg, 'TextColor', 'k');
+    end
+    grid on;
+    set(gca, 'Color', 'w', 'XColor', 'k', 'YColor', 'k', 'FontSize', 10, 'FontName', 'Helvetica', 'LineWidth', 0.8, 'GridAlpha', 0.2);
+end
